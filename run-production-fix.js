660a77afadd1964e574c3d9112f1a65e5149941c
Nicholas Rios <nicholas.jay.rios@gmail.com>
2025-09-23 08:24:53 -0500

    Add Railway production fix runner script

---
+++ run-production-fix.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/**
+ * Run Production Model Fix
+ * 
+ * This script can be run on Railway to fix the model configurations.
+ * It imports and runs the fix script.
+ */
+
+import './scripts/fix-production-model-configs.js';
